64060913e4f5379b5eaa33a3673330015591648f
ddmilo <danmilo.dev@gmail.com>
2018-05-23 06:30:52 -0400

    Completed sample test suites, writing walkthrough with commented lines but will be moving them to the README with screenshots later today

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Link from './components/Link';
+
+class App extends Component {
+  state = {
+    pressed: false
+  }
+
+  buttonClick = () => {
+    this.setState({
+      pressed: true
+    })
+  }
+  render() {
+    const foo = this.state.pressed
+    return (
+      <div className="App">
+
+        {foo ?
+          (<h1>HELLO</h1>) : (null) 
+        }
+        
+        <button onClick={this.buttonClick}>Press Me </button>
+
+        <Link />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Link.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Link extends Component {
+    render() {
+        return (
+            <div>
+                <a href=''>Link Component</a>
+            </div>
+        );
+    }
+}
+
+export default Link;
